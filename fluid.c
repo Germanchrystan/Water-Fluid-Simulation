f2e4575ade1529279e98c500e1b8b15acab53bd6
Germanchrystan <germanchrystangmail.com>
2025-10-05 21:37:23 -0300

    part 2 done

--- fluid.c
+++ fluid.c
@@ -2,13 +2,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_video.h>
 
-
 #define SCREEN_WIDTH 900
 #define SCREEN_HEIGHT 600
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_GRAY 0x0f0f0f0f
-#define COLOR_BLACK 0x000000FF
-#define COLOR_BLUE 0x34c4eb
+#define COLOR_BLACK 0x00000000
+#define COLOR_BLUE 0x34c4ebff
 #define CELL_SIZE 10
 #define LINE_WIDTH 2
 #define COLUMNS SCREEN_WIDTH / CELL_SIZE
@@ -17,106 +16,175 @@
 #define SOLID_TYPE 1
 #define WATER_TYPE 0
 
-struct Cell {
+struct Cell
+{
   int type;
   double fill_level; // Between 0 and 1
   int x;
   int y;
 };
 
-void initialize_environment(struct Cell environment[ROWS * COLUMNS]) {
-  for (int i = 0; i<ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      environment[j + COLUMNS*i] = (struct Cell){ WATER_TYPE, 0, j, i};
+struct CellFlow
+{
+  double flow_left;
+  double flow_right;
+  double flow_up;
+  double flow_down;
+};
+
+void initialize_environment(struct Cell environment[ROWS * COLUMNS])
+{
+  for (int i = 0; i < ROWS; i++)
+  {
+    for (int j = 0; j < COLUMNS; j++)
+    {
+      environment[j + COLUMNS * i] = (struct Cell){WATER_TYPE, 0, j, i};
     }
   }
 }
 
-void draw_grid(SDL_Surface* surface, Uint32 color) {
- for (int i = 0; i < COLUMNS; i++) {
-    SDL_Rect column = (SDL_Rect) { i*CELL_SIZE, 0, LINE_WIDTH, SCREEN_HEIGHT };
-    SDL_FillRect(surface, &column, color);
+void draw_grid(SDL_Surface *surface)
+{
+  for (int i = 0; i < COLUMNS; i++)
+  {
+    SDL_Rect column = (SDL_Rect){i * CELL_SIZE, 0, LINE_WIDTH, SCREEN_HEIGHT};
+    SDL_FillRect(surface, &column, COLOR_GRAY);
   }
 
-  for (int j = 0; j < ROWS; j++) {
-    SDL_Rect row = (SDL_Rect) { 0, j*CELL_SIZE, SCREEN_WIDTH, LINE_WIDTH };
-    SDL_FillRect(surface, &row, color);
+  for (int j = 0; j < ROWS; j++)
+  {
+    SDL_Rect row = (SDL_Rect){0, j * CELL_SIZE, SCREEN_WIDTH, LINE_WIDTH};
+    SDL_FillRect(surface, &row, COLOR_GRAY);
   }
 }
 
-void draw_cell(SDL_Surface* surface, struct Cell cell) {
+void draw_cell(SDL_Surface *surface, struct Cell cell)
+{
   int pixel_x = cell.x * CELL_SIZE;
   int pixel_y = cell.y * CELL_SIZE;
-  SDL_Rect cell_rect = (SDL_Rect) {pixel_x, pixel_y, CELL_SIZE, CELL_SIZE};
-  // Background color
+  SDL_Rect cell_rect = (SDL_Rect){pixel_x, pixel_y, CELL_SIZE, CELL_SIZE};
   SDL_FillRect(surface, &cell_rect, COLOR_BLACK);
-  // Water fill level
-  if (cell.type == WATER_TYPE) {
+
+  if (cell.type == WATER_TYPE)
+  {
     int water_height = cell.fill_level * CELL_SIZE;
     int empty_height = CELL_SIZE - water_height;
-    SDL_Rect water_rect = (SDL_Rect) {pixel_x, pixel_y + empty_height, CELL_SIZE, water_height};
+    SDL_Rect water_rect = (SDL_Rect){pixel_x, pixel_y + empty_height, CELL_SIZE, water_height};
     SDL_FillRect(surface, &water_rect, COLOR_BLUE);
   }
   // Solid blocks
-  if (cell.type == SOLID_TYPE) {
+  if (cell.type == SOLID_TYPE)
+  {
     SDL_FillRect(surface, &cell_rect, COLOR_WHITE);
   }
 }
 
-void draw_environment(SDL_Surface* surface, struct Cell environment[ROWS * COLUMNS]) {
-  for (int i = 0; i < ROWS * COLUMNS; i++) {
+void draw_environment(SDL_Surface *surface, struct Cell environment[ROWS * COLUMNS])
+{
+  for (int i = 0; i < ROWS * COLUMNS; i++)
+  {
     draw_cell(surface, environment[i]);
   }
 }
 
-int main() {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+void simulation_step(struct Cell environment[ROWS * COLUMNS])
+{
+  struct Cell environment_copy[ROWS * COLUMNS];
+  struct CellFlow flows[ROWS * COLUMNS];
+  for (int i = 0; i < ROWS * COLUMNS; i++)
+  {
+    flows[i] = (struct CellFlow){0, 0, 0, 0};
+  }
+  // Water should drop to the cell below
+  for (int i = 0; i < ROWS; i++)
+  {
+    for (int j = 0; j < COLUMNS; j++)
+    {
+      struct Cell current_cell = environment[j + COLUMNS * i];
+      if (current_cell.type == WATER_TYPE && i < ROWS - 1)
+      {
+        if (environment[j + COLUMNS * i].fill_level != 0)
+        {
+          flows[j + COLUMNS * i].flow_down = flows[j + COLUMNS * i].flow_down = 1;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < ROWS; i++)
+  {
+    for (int j = 0; j < COLUMNS; j++)
+    {
+      if (i > 0)
+      {
+        struct CellFlow cell_above_flow = flows[j + COLUMNS * (i - 1)];
+        environment[j+COLUMNS*i].fill_level += cell_above_flow.flow_down;
+        environment[j+COLUMNS*(i-1)].fill_level -= cell_above_flow.flow_down;
+      }
+    }
+  }
+}
+
+int main()
+{
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
     printf("SDL_Init Error: %s\n", SDL_GetError());
     return 1;
   }
-  SDL_Window* window = SDL_CreateWindow("Fluid Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  SDL_Window *window = SDL_CreateWindow("Fluid Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
   SDL_ShowWindow(window);
-  SDL_Surface* surface = SDL_GetWindowSurface(window);
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
 
-  Uint32 gray = SDL_MapRGB(surface->format, 15, 15, 15);
-  struct Cell enviroment [ROWS * COLUMNS];
-  initialize_environment(enviroment);
-  
+  struct Cell environment[ROWS * COLUMNS];
+  initialize_environment(environment);
+
   int simulation_running = 1;
   SDL_Event event;
   int current_type = SOLID_TYPE;
   int delete_mode = 0;
-  while (simulation_running) {
-    while(SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
+  while (simulation_running)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT)
+      {
         simulation_running = 0;
       }
-      if (event.type == SDL_MOUSEMOTION) {
-        if (event.motion.state != 0) {
+      if (event.type == SDL_MOUSEMOTION)
+      {
+        if (event.motion.state != 0)
+        {
           int cell_x = event.motion.x / CELL_SIZE;
           int cell_y = event.motion.y / CELL_SIZE;
           int fill_level = delete_mode ? 0 : 1;
-          
-          if (delete_mode != 0) current_type = WATER_TYPE;
+
+          if (delete_mode != 0)
+            current_type = WATER_TYPE;
           struct Cell cell = {current_type, fill_level, cell_x, cell_y};
-          
-          enviroment[cell_x + COLUMNS * cell_y] = cell;
+
+          environment[cell_x + COLUMNS * cell_y] = cell;
         }
       }
-      if (event.type == SDL_KEYDOWN) {
-        if (event.key.keysym.sym == SDLK_SPACE) {
+      if (event.type == SDL_KEYDOWN)
+      {
+        if (event.key.keysym.sym == SDLK_SPACE)
+        {
           current_type = !current_type;
         }
-        if (event.key.keysym.sym == SDLK_BACKSPACE) {
+        if (event.key.keysym.sym == SDLK_BACKSPACE)
+        {
           delete_mode = !delete_mode;
         }
       }
     }
 
     // Perform simulation steps
-    
-    draw_environment(surface, enviroment);
-    draw_grid(surface, gray);
+    simulation_step(environment);
+
+    draw_environment(surface, environment);
+    draw_grid(surface);
+    SDL_Delay(16); // Roughly 60 FPS
     SDL_UpdateWindowSurface(window);
   }
 
